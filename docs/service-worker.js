0449389613f10af3c6d219dcea19de29ea736eb0
amazingandyyy <amazingandyyy@gmail.com>
2020-12-12 08:44:22 +0000

    Update from https://github.com/HODLCommunity-Project/HETHUSD-ui/commit/9b57dc789e19d3081fd14e5abc9b83d1165afa86

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53398d26377756b2d2ae380838f6e234.js"
+  "./precache-manifest.d4fc12f864aae2d0e3d708a9510dd34c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
