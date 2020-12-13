9f81eb3cb0e3b0c8eebfa629023aadec5c4eb989
Andy Chen <amazingandyyy@gmail.com>
2020-12-12 21:18:00 -0800

    deploy

---
+++ docs/precache-manifest.fd49d9cb1e22ca35100eab93218aabe8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "db4b1ebf382e212bbf35",
+    "url": "./static/css/main.822f191c.chunk.css"
+  },
+  {
+    "revision": "db4b1ebf382e212bbf35",
+    "url": "./static/js/main.db4b1ebf.chunk.js"
+  },
+  {
+    "revision": "d26abb124c1c4729074f",
+    "url": "./static/css/1.e655d358.chunk.css"
+  },
+  {
+    "revision": "d26abb124c1c4729074f",
+    "url": "./static/js/1.d26abb12.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "./static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "a0103549b5dca7100830a06fc1a09cb9",
+    "url": "./static/media/metamask.a0103549.svg"
+  },
+  {
+    "revision": "dac33c9707d8ddea1a4e73de9e0eb98e",
+    "url": "./static/media/walletConnect.dac33c97.svg"
+  },
+  {
+    "revision": "58a1b8818b1a133c6eda29d298cca89c",
+    "url": "./static/media/eth-lg.58a1b881.svg"
+  },
+  {
+    "revision": "1268123a064c93f1b80529d7f58f2020",
+    "url": "./static/media/hodl_badge.1268123a.svg"
+  },
+  {
+    "revision": "28545d8f1e8ffbaaaf10c2f5988c16c5",
+    "url": "./static/media/hodlc-lg.28545d8f.svg"
+  },
+  {
+    "revision": "739eb58cb7fde5b0aa6d77630ccf27e4",
+    "url": "./static/media/hodlt-lg.739eb58c.svg"
+  },
+  {
+    "revision": "4b35d02921dd15f5a4aed7f546d7022c",
+    "url": "./static/media/calculator-page.4b35d029.svg"
+  },
+  {
+    "revision": "06f0f42da947ba191f71ed07f647c855",
+    "url": "./static/media/eth.06f0f42d.svg"
+  },
+  {
+    "revision": "905c0ecc33e987702635949e1811d1ae",
+    "url": "./static/media/explorer-page.905c0ecc.svg"
+  },
+  {
+    "revision": "90841a8d00c074065f4cef10986a63a3",
+    "url": "./static/media/facebook-icon.90841a8d.svg"
+  },
+  {
+    "revision": "2f8f35adcbbb47ea280dbf1c6628a3c6",
+    "url": "./static/media/hodlc.2f8f35ad.svg"
+  },
+  {
+    "revision": "3e9179ad7d412665f6db2a91892c0beb",
+    "url": "./static/media/hodlt.3e9179ad.svg"
+  },
+  {
+    "revision": "7f464f9848762398f6d62b47975e24dc",
+    "url": "./static/media/medium-icon.7f464f98.svg"
+  },
+  {
+    "revision": "2e12a1bbf599aaa8983dead382d6e332",
+    "url": "./static/media/pin.2e12a1bb.svg"
+  },
+  {
+    "revision": "221c6642bb1f97a6bea2a0c670d9cb9e",
+    "url": "./static/media/setting.221c6642.svg"
+  },
+  {
+    "revision": "64a2ef996ce402633703d2767dc1d9a7",
+    "url": "./static/media/telegram-icon.64a2ef99.svg"
+  },
+  {
+    "revision": "67a926a3a331b42b809deb2ef3ca3682",
+    "url": "./static/media/trade-page.67a926a3.svg"
+  },
+  {
+    "revision": "d037de3eac2623fd2ec1ff62ffbea17e",
+    "url": "./static/media/twitter-icon.d037de3e.svg"
+  },
+  {
+    "revision": "d29c7279a7dbe99fb6cf307d32fedc45",
+    "url": "./static/media/wallet-page.d29c7279.svg"
+  },
+  {
+    "revision": "02b6e4bd72b8f3c3e5ba26aedfcd0716",
+    "url": "./static/media/youtube-icon.02b6e4bd.svg"
+  },
+  {
+    "revision": "532411d7451836bbbc0abb9ae31b1900",
+    "url": "./index.html"
+  }
+];
