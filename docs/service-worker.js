b0f58efda92f20245b8c6230bd53c79e9c217525
amazingandyyy <amazingandyyy@gmail.com>
2021-02-22 04:12:51 +0000

    Update from https://github.com/HODLCommunity-Project/HETHUSD-ui/commit/5aa3ede34843e8ec15ccf56fe7fa04efd509b4f9

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.82942712ddf64f4c08dc5f2252e005df.js"
+  "./precache-manifest.315d0eaba169aa1fbc17a3a3a3bfc08e.js"
 );
 
 workbox.clientsClaim();
